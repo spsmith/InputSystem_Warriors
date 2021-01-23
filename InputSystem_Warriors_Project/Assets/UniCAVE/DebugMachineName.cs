dd3e721868706ff1b1a55c631643a6556b3d7934
Simon <spsmith5@wisc.edu>
2021-01-22 18:20:29 -0600

    Implementing UniCAVE features

---
+++ InputSystem_Warriors_Project/Assets/UniCAVE/DebugMachineName.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugMachineName : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI Text;
+
+	void Start()
+	{
+		if(Text)
+		{
+			Text.text = UniCAVE.Util.GetMachineName();
+		}
+	}
+}
